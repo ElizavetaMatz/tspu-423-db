926a5518e053c0f6c3316ffaa1f7aa93d7a21f53
ElizavetaMatz <124855387+ElizavetaMatz@users.noreply.github.com>
2024-05-15 23:35:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,38 @@ import sqlite3
 
 # Создаем подключение к базе данных (файл my_database.db будет создан)
 connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
 
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+
+connection.commit()
 connection.close()
