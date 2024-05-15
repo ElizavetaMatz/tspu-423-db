f33a634684a65587e6ba6b4fd976f810161d533b
ElizavetaMatz <124855387+ElizavetaMatz@users.noreply.github.com>
2024-05-15 23:46:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,36 +4,39 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Создаем таблицу Users
+# Создаем таблицу Пользователи
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
+CREATE TABLE IF NOT EXISTS Пользователи (
+id INT(11) NOT NULL AUTO_INCREMENT,
+Фамилия VARCHAR(60) NOT NULL,
+Имя VARCHAR(60) NOT NULL,
+Отчество VARCHAR(60), 
+Дата рождения DATE NOT NULL,
+Электронная почта VARCHAR(50) NOT NULL,
+Телефон СHAR(20),
+Форма обучения ENUM("очная", "заочная", "очно-заочная") NOT NULL,
+Статус TINYINT(1) NOT NULL DEFAULT = 1,
+Пароль VARCHAR(32) NOT NULL,
+ID_группы INT(11) NOT NULL
 )
 ''')
 
-# Создаем таблицу Users
+# Создаем таблицу Факультет
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
+CREATE TABLE IF NOT EXISTS Факультет (
+id INT(11) NOT NULL AUTO_INCREMENT,
+Название VARCHAR(100) NOT NULL
 )
 ''')
 
-# Создаем таблицу Users
+# Создаем таблицу Группа
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
+CREATE TABLE IF NOT EXISTS Группа (
+id INT(11) NOT NULL AUTO_INCREMENT,
+Название VARCHAR(100) NOT NULL,
+ID_факультета INT(11) NOT NULL 
 )
 ''')
-
 
 connection.commit()
 connection.close()
